a1ffedd806a07a6f26116d03757af1abc698201a
ahsatan <13985000+ahsatan@users.noreply.github.com>
2025-04-13 12:39:56 -0700

    Generate user auth

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :polish_me, :scopes,
+  user: [
+    default: true,
+    module: PolishMe.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: PolishMe.AccountsFixtures,
+    test_login_helper: :register_and_log_in_user
+  ]
+
 config :polish_me,
   ecto_repos: [PolishMe.Repo],
   generators: [timestamp_type: :utc_datetime]
