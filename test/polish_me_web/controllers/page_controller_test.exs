1facf37f93968cc0d474987ba248a9fa87e40d29
ahsatan <13985000+ahsatan@users.noreply.github.com>
2025-04-18 10:10:31 -0700

    Add hero homepage and tweak menu, with tests

--- test/polish_me_web/controllers/page_controller_test.exs
+++ test/polish_me_web/controllers/page_controller_test.exs
@@ -3,6 +3,25 @@ defmodule PolishMeWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    response = html_response(conn, 200)
+
+    assert response =~ "Welcome to Polish.me!"
+    assert response =~ "Log In or Register"
+    assert response =~ "Log in to browse!"
+    refute response =~ "Brands"
+  end
+
+  describe "With User" do
+    setup [:register_and_log_in_user]
+
+    test "GET /", %{conn: conn} do
+      conn = get(conn, ~p"/")
+      response = html_response(conn, 200)
+
+      assert response =~ "Welcome to Polish.me!"
+      assert response =~ "My Stash"
+      assert response =~ "Brands"
+      refute response =~ "Log in to browse!"
+    end
   end
 end
